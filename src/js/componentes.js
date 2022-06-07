7594d5256d82dcc5be3c05421ad45dc71ae8e6ca
Sebastian Rodriguez <sebaser99@gmail.com>
2022-06-06 21:09:30 -0500

    primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,15 @@
+import '../css/componentes.css';
+import webpack from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre)=> {
+    console.log("Creando etiqueta h1")
+    const h1 =  document.createElement('h1');
+    h1.innerText = `Hola ${nombre}`;
+    document.body.append(h1)
+
+    //img
+    const img = document.createElement('img');
+    img.src = webpack;
+    document.body.append(img);
+    
+};
